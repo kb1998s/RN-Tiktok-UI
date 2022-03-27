38ce077efbf6af563674b8ae76bac2b2ab5f389c
Vito <luongv@cwu.edu>
2022-03-26 21:58:24 -0700

    add cat videos

--- assets/data/constant.js
+++ assets/data/constant.js
@@ -2,10 +2,49 @@ import { Dimensions } from 'react-native'
 
 export const { width: windowWidth, height: windowHeight } = Dimensions.get('window');
 export const data = [
+
     {
       id: 1,
+      channelName: 'mycatsareombre',
+      uri: 'https://v16m-webapp.tiktokcdn-us.com/906ea204bc5f1cf1dfcf77ce1b2daa84/6240423c/video/tos/useast5/tos-useast5-ve-0068c002-tx/505f471d3ef8412292e417dc8939f55e/?a=1988&br=1270&bt=635&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7ThBudJDXq&l=20220327045338010113135233103EAF1F&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3M8ZDs6ZmlxOjMzZzczNEApOWU0OTU2Z2UzNzk5OGk1NWdkbG5gcjRfcWdgLS1kMS9zczRjLjFjNWIuMDYxMzUzYTA6Yw%3D%3D&vl=&vr=',
+      // uri: '/c423278e49b1471f99044e916af459ca/623fec61/video/tos/useast2a/tos-useast2a-ve-0068c001/ccdc8c856a564d7783a2bfa086614b11/?a=1988&br=1608&bt=804&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7ThAnMJDXq&l=202203262247140101130062180F065B03&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=MzlsNGg6Zjc0ODMzNzczM0ApNGk6ZDdoNWU7NzVkN2Q7N2dnb3BecjQwa3BgLS1kMTZzcy1iLzBeXzY2NS5iNDRjMzU6Yw%3D%3D&vl=&vr=',
+      caption: 'All the wiggles #kittensoftiktok #graytabby',
+      musicName: 'Wiggle - Jason Derulo',
+      likes: '2.1M',
+      comments: '1K',
+      avatarUri: 'https://p19-sign.tiktokcdn-us.com/tos-useast5-avt-0068-tx/56ae5b8f0d2a681b7c1bcf5cb030ce09~c5_100x100.jpeg?x-expires=1648440000&x-signature=LOsS0xfMjI1jybnPuxzMb1zL4%2BM%3D',
+    },
+
+    {
+      id: 2,
+      channelName: 'thomaswithyou',
+      uri: 'https://v16m-webapp.tiktokcdn-us.com/c423278e49b1471f99044e916af459ca/623fec61/video/tos/useast2a/tos-useast2a-ve-0068c001/ccdc8c856a564d7783a2bfa086614b11/?a=1988&br=1608&bt=804&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7ThAnMJDXq&l=202203262247140101130062180F065B03&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=MzlsNGg6Zjc0ODMzNzczM0ApNGk6ZDdoNWU7NzVkN2Q7N2dnb3BecjQwa3BgLS1kMTZzcy1iLzBeXzY2NS5iNDRjMzU6Yw%3D%3D&vl=&vr=',
+      // uri: '/c423278e49b1471f99044e916af459ca/623fec61/video/tos/useast2a/tos-useast2a-ve-0068c001/ccdc8c856a564d7783a2bfa086614b11/?a=1988&br=1608&bt=804&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7ThAnMJDXq&l=202203262247140101130062180F065B03&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=MzlsNGg6Zjc0ODMzNzczM0ApNGk6ZDdoNWU7NzVkN2Q7N2dnb3BecjQwa3BgLS1kMTZzcy1iLzBeXzY2NS5iNDRjMzU6Yw%3D%3D&vl=&vr=',
+      caption: '#pet #foryou #cat #catlover #🐱 #⁉️⁉️⁉️',
+      musicName: 'original sound - Meow Institute😻',
+      likes: 3555,
+      comments: 21,
+      avatarUri: 'https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/e5e3489a405222c379d9b9b32806a5ab~c5_100x100.jpeg?x-expires=1648422000&x-signature=IEV1Y9AFAhIXm9%2Bo0pMzwIxk5iA%3D',
+    },
+
+    {
+      id: 3,
+      channelName: 'safarisammie',
+      uri: 'https://v16m-webapp.tiktokcdn-us.com/1f67c0ada990f938089e17b4dcf8fa44/62403efa/video/tos/useast2a/tos-useast2a-pve-0068/22e84a2c16aa40bc918f5e7c8fc7ef67/?a=1988&br=6062&bt=3031&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7ThPidJDXq&l=20220327043934010113135134168E24CA&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M2lxbjw6ZnZ1OTMzNzczM0ApNGk2NmU5ZDw4N2g2NzY1N2c0ZDYwcjRvNGVgLS1kMTZzcy02LjVfLWM1MmIuYmE2Yi06Yw%3D%3D&vl=&vr=',
+      // uri: '/c423278e49b1471f99044e916af459ca/623fec61/video/tos/useast2a/tos-useast2a-ve-0068c001/ccdc8c856a564d7783a2bfa086614b11/?a=1988&br=1608&bt=804&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7ThAnMJDXq&l=202203262247140101130062180F065B03&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=MzlsNGg6Zjc0ODMzNzczM0ApNGk6ZDdoNWU7NzVkN2Q7N2dnb3BecjQwa3BgLS1kMTZzcy1iLzBeXzY2NS5iNDRjMzU6Yw%3D%3D&vl=&vr=',
+      caption: '#bigcat #catlover',
+      musicName: 'original sound - Safari Sammie',
+      likes: 3221,
+      comments: 211,
+      avatarUri: 'https://p16-sign.tiktokcdn-us.com/tos-useast5-avt-0068-tx/6bead09028b93dcfa58316d5f9a33a52~c5_100x100.jpeg?x-expires=1648440000&x-signature=YbSGK8Y5%2Fo7hW7X52aPadPHHyY4%3D',
+    },
+
+
+    {
+      id: 4,
       channelName: 'cutedog',
       uri: 'https://v.pinimg.com/videos/mc/720p/f6/88/88/f68888290d70aca3cbd4ad9cd3aa732f.mp4',
+      // uri: '/c423278e49b1471f99044e916af459ca/623fec61/video/tos/useast2a/tos-useast2a-ve-0068c001/ccdc8c856a564d7783a2bfa086614b11/?a=1988&br=1608&bt=804&cd=0%7C0%7C1%7C0&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=XY53A3E7nz7ThAnMJDXq&l=202203262247140101130062180F065B03&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=MzlsNGg6Zjc0ODMzNzczM0ApNGk6ZDdoNWU7NzVkN2Q7N2dnb3BecjQwa3BgLS1kMTZzcy1iLzBeXzY2NS5iNDRjMzU6Yw%3D%3D&vl=&vr=',
       caption: 'Cute dog shaking hands #cute #puppy',
       musicName: 'Song #1',
       likes: 4321,
@@ -13,7 +52,7 @@ export const data = [
       avatarUri: 'https://wallpaperaccess.com/full/1669289.jpg',
     },
     {
-      id: 2,
+      id: 5,
       channelName: 'meow',
       uri: 'https://v.pinimg.com/videos/mc/720p/11/05/2c/11052c35282355459147eabe31cf3c75.mp4',
       caption: 'Doggies eating candy #cute #puppy',
@@ -23,7 +62,7 @@ export const data = [
       avatarUri: 'https://wallpaperaccess.com/thumb/266770.jpg',
     },
     {
-      id: 3,
+      id: 6,
       channelName: 'yummy',
       uri: 'https://v.pinimg.com/videos/mc/720p/c9/22/d8/c922d8391146cc2fdbeb367e8da0d61f.mp4',
       caption: 'Brown little puppy #cute #puppy',
